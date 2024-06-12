370fc4667a6bc2e2da7682b743cdbdb4d1c64353
Egasuas <callumgvp@gmail.com>
2024-06-12 20:32:24 +0100

    disabled extra layer

--- js/layerConfig.js
+++ js/layerConfig.js
@@ -26,7 +26,7 @@ const layerConfigs = {
         ['coin',        new LayerConfig('markers', 'Coins',          'awesome',       false, ['sl', 'slc', 'siu'])],
         ['graves',      new LayerConfig('markers', 'Graves',         'question_mark', false, ['sl'              ])],
         ['coordinate',  new LayerConfig('markers', 'XY',             null,            false, ['sl', 'slc', 'siu'])],
-        ['extra',       new LayerConfig('markers', 'Extras',         'question_mark', false, ['sl', 'slc', 'siu'])],
+        //['extra',       new LayerConfig('markers', 'Extras',         'question_mark', false, ['sl', 'slc', 'siu'])],
     ]),
 
     // Initialise any procedural data - store index based on position in Map
