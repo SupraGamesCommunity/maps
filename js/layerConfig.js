fe2f39681a6e243a84cbebc1cd08ef9c0b2458d4
Egasuas <callumgvp@gmail.com>
2024-06-14 00:52:58 +0100

    updated SLC pipes/pads
    also removed pads from SIU and pipes from SLC because there didn't appear to be any for fast travel

--- js/layerConfig.js
+++ js/layerConfig.js
@@ -16,8 +16,8 @@ class LayerConfig {
 /* exported layerConfigs */
 const layerConfigs = {
     data: new Map([
-        ['pipesys',     new LayerConfig('markers', 'Pipes',          'pipe',          false, ['sl', 'slc', 'siu'])],
-        ['jumppads',    new LayerConfig('markers', 'Pads',           'jumppad',       false, ['sl', 'slc', 'siu'])],
+        ['pipesys',     new LayerConfig('markers', 'Pipes',          'pipe',          false, ['sl',      , 'siu'])],
+        ['jumppads',    new LayerConfig('markers', 'Pads',           'jumppad',       false, ['sl', 'slc',      ])],
         ['closedChest', new LayerConfig('markers', 'Chests',         'chest',         true,  ['sl', 'slc', 'siu'])],
         ['shop',        new LayerConfig('markers', 'Shop',           'shop',          true,  ['sl', 'slc', 'siu'])],
         ['collectable', new LayerConfig('markers', 'Collectables',   'question_mark', true,  ['sl', 'slc', 'siu'])],
