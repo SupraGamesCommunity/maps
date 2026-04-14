331f9b757c9ff513fc1685270f8dec30df4ace8a
James Russell <jrussell@redhat.com>
2026-04-13 19:55:40 -0700

    Github Actions workflow to build and publish the pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/supramaps/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
